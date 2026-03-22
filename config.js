01554b58219c7fe4fb06255b1a8bb75864e5840c
Yousv <68682169+yousv@users.noreply.github.com>
2026-03-22 06:17:08 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js — GITIGNORED. Never commit this file.
-// Copy config.example.js to create this file.
-const CONFIG = {
-  clientId: '674690883302-88mtqhf70clduphrnbkqhsqs1qvlmdgc.apps.googleusercontent.com',
-  sheetId:  '1zj-Yr82dbEklm62f0xgRTjouYjZmTe9-Vw4pS8rtrSo',
-};
